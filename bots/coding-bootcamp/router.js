f208512dc68315909f7db8f3580056b83cd2fb9a
mnindrazaka <mnindrazaka@gmail.com>
2018-09-16 15:53:54 +0700

    fix callback route

--- bots/coding-bootcamp/router.js
+++ bots/coding-bootcamp/router.js
@@ -3,6 +3,6 @@ const line = require('@line/bot-sdk')
 const config = require('./config.json')
 const callback = require('./controller')
 
-router.post('/callback', line.middleware(config), callback)
+router.post('/', line.middleware(config), callback)
 
 module.exports = router
