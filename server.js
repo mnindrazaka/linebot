203b910f71a3bc8835db9d0d641f96408628f24e
mnindrazaka <mnindrazaka@gmail.com>
2018-10-02 13:00:20 +0700

    / endpoint

--- server.js
+++ server.js
@@ -6,3 +6,5 @@ app.listen(port, () => console.log('Server Started...'))
 
 const coding_bootcamp_routes = require('./routes/coding-bootcamp/router')
 app.use('/coding_bootcamp', coding_bootcamp_routes)
+
+app.get('/', (req, res) => res.send('Mnindrazaka Bot'))
